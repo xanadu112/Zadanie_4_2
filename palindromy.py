79fe0c943111a860852d40f8c4f9e1b033f15272
Michał Zatorski <michal_zatorski@o2.pl>
2024-03-20 18:50:12 +0100

    Correct

--- palindromy.py
+++ palindromy.py
@@ -1,6 +1,6 @@
 # funkcja, która sprawdza czy dany wyraz jest palindromem
 def palindrom(pal_word):
-    return(pal_word == pal_word[::-1])
+    return pal_word == pal_word[::-1]
 
 print(palindrom(""))
         
